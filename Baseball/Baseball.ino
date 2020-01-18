8b93d94575d082c67de10aa28b5e9ca0657471c5
pat-stach <32009672+pat-stach@users.noreply.github.com>
2020-01-18 04:38:37 -0600

    Fixed button registration all cases, need to change debug prints, add button LED on/off for generic case instead of 6

--- Baseball/Baseball.ino
+++ Baseball/Baseball.ino
@@ -12,7 +12,7 @@
 
 
 
-
+#define none_pressed  0b11111000
 
 #include <SPI.h>
 class input_SR
@@ -20,13 +20,38 @@ class input_SR
   public:
     byte shift_1;
     byte shift_2;
+    byte activeButtonCode;
+
+    int t1_LED;
+    int t2_LED;
+
 
     int pressType;
+    int pressHold;
+
     int buttonPressed;
     int buttonPressedTeam;
-    
-    int PL;
 
+    int PL;
+    int t1pin;
+    int t2pin;
+
+    void debugPress(int ignore_none)
+    {
+      if (buttonPressed != 0) {
+        Serial.println("-----------------------------");
+        Serial.print("Press Type: ");
+        Serial.println(pressType);
+        Serial.print("Press hold: ");
+        Serial.println(pressHold);
+
+        Serial.print("Button Pressed: ");
+        Serial.println(buttonPressed);
+        Serial.print("Button Pressed Team: ");
+        Serial.println(buttonPressedTeam);
+        Serial.println("-----------------------------");
+      }
+    }
     void DataShiftIn() {
       digitalWrite(PL, LOW);
       delay(1);
@@ -35,92 +60,166 @@ class input_SR
       shift_2 = SPI.transfer(0x00);
     }
 
-    
-    void updateStates() {
-     
-    }
+
+
 
 
     void init()
     {
-      pressType=0;
-      buttonPressed=0;
-      buttonPressedTeam=-1;
+      pressType = 0;
+      pressHold = 0;
+      buttonPressed = 0;
+      buttonPressedTeam = 0;
+
+      t1_LED = 100;
+      t2_LED = 100;
     }
-    
-    
-    input_SR(int _PL) : PL(_PL) {
+
+
+    input_SR(int _PL, int _t1pin, int _t2pin) : PL(_PL), t1pin(_t1pin), t2pin(_t2pin)  {
       pinMode(PL, OUTPUT);
+      pinMode(t1pin, OUTPUT);
+      pinMode(t2pin, OUTPUT);
+
       SPI.setClockDivider(SPI_CLOCK_DIV128);
       SPI.setDataMode(SPI_MODE0);
       SPI.setBitOrder(MSBFIRST);
       SPI.begin();
       init();
     }
-   
-   int getPressType() {
+
+    void set_LED_brightness(int t1_amt, int t2_amt)
+    {
+      t1_LED = (t1_amt != -1) ? t1_amt : t1_LED;
+      t2_LED = (t2_amt != -1) ? t2_amt : t2_LED;
+      analogWrite(t1pin, t1_LED);
+      analogWrite(t2pin, t2_LED);
+    }
+
+    int currButtonPressed()
+    {
+      if (buttonPressedTeam == 1) return !bitRead(shift_1, 8 - buttonPressed);
+      else if (buttonPressedTeam == 2) return !bitRead(shift_2, 8 - buttonPressed);
+      else return -1;
+    }
+
+    bool timeThresholdMet( unsigned long time_pressed, unsigned long time_threshold)
+    {
+      //Serial.println(((millis() - time_pressed) > time_threshold));
+      return ((millis() - time_pressed) > time_threshold);
+    }
+
+    bool timeThresholdMet( unsigned long time_pressed, unsigned long time_threshold_begin, unsigned long time_threshold_end )
+    { //Serial.println((((millis() - time_pressed) > time_threshold_begin) && ((millis() - time_pressed) < time_threshold_end)));
+      return (((millis() - time_pressed) > time_threshold_begin) && ((millis() - time_pressed) < time_threshold_end));
+    }
+    void extractButtonInfo()
+    {
+      if (shift_1 <= 0b01111000) {
+        buttonPressedTeam = 1;
+        Serial.println("Debug");
+        buttonPressed = 1;
+      }
+      else if (shift_1 <= 0b10111000) {
+        buttonPressedTeam = 1;
+        buttonPressed = 2;
+      }
+      else if (shift_1 <= 0b11011000) {
+        buttonPressedTeam = 1;
+        buttonPressed = 3;
+      }
+      else if (shift_1 <= 0b11101000) {
+        buttonPressedTeam = 1;
+        buttonPressed = 4;
+      }
+      else if (shift_1 <= 0b11110000) {
+        buttonPressedTeam = 1;
+        buttonPressed = 5;
+      }
+
+      else if (shift_2 <= 0b01111000) {
+        buttonPressedTeam = 2;
+        buttonPressed = 1;
+      }
+      else if (shift_2 <= 0b10111000) {
+        buttonPressedTeam = 2;
+        buttonPressed = 2;
+      }
+      else if (shift_2 <= 0b11011000) {
+        buttonPressedTeam = 2;
+        buttonPressed = 3;
+      }
+      else if (shift_2 <= 0b11101000) {
+        buttonPressedTeam = 2;
+        buttonPressed = 4;
+      }
+      else if (shift_2 <= 0b11110000) {
+        buttonPressedTeam = 2;
+        buttonPressed = 5;
+      }
+      else {
+        buttonPressedTeam = 0;
+        buttonPressed = 0;
+      }
+      if (buttonPressedTeam == 1) activeButtonCode = shift_1;
+      else if (buttonPressedTeam == 2) activeButtonCode = shift_2;
+      else activeButtonCode = 0;
+    }
+
+
+
+
+    void getPressType() {
       DataShiftIn();
-     unsigned long time_pressed;
-     unsigned long new_time_pressed;
-     int tmp;
-     if(shift_1 != 0b11111000) {
-        tmp=1;
+      // shift_1 and shift_2 recognized
+      extractButtonInfo();
+      unsigned long time_pressed;
+      //debugPress(0);
+
+      pressType = 0;
+      pressHold = 0;
+
+      if (buttonPressedTeam != 0)
+      {
         bool button_release = false;
-       //Serial.println(shift_1, BIN);
-       time_pressed= millis();
-       new_time_pressed= millis();
-       analogWrite(6, 100);
-       if(shift_1 <= 0b01111000) {
-        while(shift_1 <= 0b01111000 || (millis() - time_pressed <400))
-        { 
-          if ((millis() - time_pressed) < 400){
-            if (shift_1 > 0b01111000) button_release = true;
-            if (button_release == true && shift_1 <= 0b01111000) {
-              tmp =2;
-                new_time_pressed= millis();
+        pressType = 1;
+        time_pressed = millis();
+        analogWrite(6, 100);
+
+        while (currButtonPressed() || !timeThresholdMet(time_pressed, 400))
+        {
+          if (!button_release)
+          {
+            if (!currButtonPressed()) {
+              button_release = true;
+              //Serial.println ("Button Released");
+
             }
           }
-          else if ((millis() - time_pressed) > 600 && !button_release)
+          if (pressType != 2)
           {
+            if (button_release == true && currButtonPressed() ) {
+              pressType = 2;
+              //Serial.println ("Double Press");
+              time_pressed = millis();
+            }
+          }
+          if (timeThresholdMet(time_pressed, 400, 600)) {
+            //Serial.println("hold");
+            analogWrite(6, 0);
+            pressHold = 1;
+          }
+          if (timeThresholdMet(time_pressed, 600)) {
             analogWrite(6, 100);
           }
-          else if ((millis() - time_pressed) > 400 && !button_release) {
-            analogWrite(6, 0);
-            tmp=4;}
-          
-           else if ((millis() - new_time_pressed) > 600 && (button_release))
-          {
-            analogWrite(6, 100);
-          }
-           else if ((millis() - new_time_pressed) > 400 && ( button_release)) {
-            analogWrite(6, 0);
-            tmp=3;}
-         
-          DataShiftIn();}
-          //Serial.println(millis() - time_pressed);
-        return tmp;
-       }
-        
-       if(shift_1 <= 0b10111000) return 2;
-       if(shift_1 <= 0b11011000) return 3;
-       if(shift_1 <= 0b11101000) return 4;
-       if(shift_1 <= 0b11110000) return 5;
-       }
-       if(shift_2 != 0b11111000) {
-       //Serial.println(shift_2, BIN);
-       if(shift_2 <= 0b01111000) {
-          ;
-         while(shift_2 <= 0b01111000){DataShiftIn();} 
-         return 1;
-       }
-       if(shift_2 <= 0b10111000) return 2;
-       if(shift_2 <= 0b11011000) return 3;
-       if(shift_2 <= 0b11101000) return 4;
-       if(shift_2 <= 0b11110000) return 5;
-       }
-       return 0;
-     
+
+          DataShiftIn();
+        }
+      }
+      //Serial.println(timeThresholdMet(time_pressed, 400));
+      debugPress(0);
     }
+
 };
 
 
@@ -342,44 +441,29 @@ class output_SR
 
 };
 
-input_SR buttons(8);
+input_SR buttons(8, 6, 5);
 hex_display hex(4, 3, 2);
 output_SR LEDs(15, 16, 17);
 void setup() {
   Serial.begin(9600);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
 
-  
-  
+
+
   analogWrite(6, 100);
 
-    Serial.println("Initialized");
+  Serial.println("Initialized");
 }
 
-int a;
+
 void loop() {
-a =buttons.getPressType();
+  buttons.getPressType();
 
-  switch (a)
-  {
-    case 0: break;
-
-    default: Serial.println(a);
-//             Serial.print("Button Pressed: ");
-//              Serial.println(buttons.buttonPressed);
-//             Serial.print("Team Button Pressed: "); 
-//              Serial.println(buttons.buttonPressedTeam);
-//             Serial.print("Press Type: ");
-//               Serial.println(buttons.pressType);
-//             Serial.print("Press Type: ");
-//               Serial.println(buttons.pressType);
-//               Serial.println();
-               break;
-  }
+  //buttons.debugPress(0);
 
 
-  //hex.showNumber(35, 4);
+
+
+  hex.showNumber(35, 4);
   delay(100);
 
 
